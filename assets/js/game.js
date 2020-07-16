2c449086399a1cf80bafdee9b8da6ead1ac4676f
Master Chief <kemar7@live.com>
2020-07-16 10:35:50 -0400

    bug fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,7 +68,7 @@ var startGame = function() {
 
   for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
-      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+      window.alert("Welcome to Battlebots! Round " + (i + 1));
 
       var pickedEnemyObj = enemyInfo[i];
 
@@ -163,8 +163,19 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is "  + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
